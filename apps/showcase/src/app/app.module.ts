fb8a4fb64dec5d2a45e30398982d8395a70b0a51
Eugene Rusakov <Rusakov>
2022-07-27 16:52:24 +0300

    add layout module

--- apps/showcase/src/app/app.module.ts
+++ apps/showcase/src/app/app.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { LayoutModule } from "./modules/layout/layout.module";
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
